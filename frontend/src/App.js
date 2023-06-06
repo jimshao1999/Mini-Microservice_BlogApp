4c2325cf6592f3befc453a4d6bd41cc034d7c7f1
jimshao <jimmy25452@gmail.com>
2023-06-07 02:32:40 +0800

    basic service structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div className="container">
+    <h1>Create Post</h1>
+    <PostCreate/>
+    <hr />
+    <PostList/>
+    </div>
+  );
+}
+
+export default App;
